aac70ecaf114e21a5dd0c464a9b5e20aeecc3c89
SS-FS-58 <ziqiangj6@gmail.com>
2022-02-23 17:26:08 +0800

    Updated giphy api key

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
+      url: 'https://eth-ropsten.alchemyapi.io/v2/omAgRjkhmb6kO3Ew2K3sVsZRXtVe1K-2',
       accounts: ['76d1c1d36812918a2518c26a7ec5d0903cbbf692511e73e8137b4424b5ad4684'],
     },
   },
